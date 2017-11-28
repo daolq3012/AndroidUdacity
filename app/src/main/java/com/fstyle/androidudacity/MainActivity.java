e218220ac0b48461185b9a639a725eea62eabc0f
daolq <le.quang.dao@framgia.com>
2017-11-28 14:56:44 +0700

    finish config circle CI

--- app/src/main/java/com/fstyle/androidudacity/MainActivity.java
+++ app/src/main/java/com/fstyle/androidudacity/MainActivity.java
@@ -3,6 +3,9 @@ package com.fstyle.androidudacity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * MainActivity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
